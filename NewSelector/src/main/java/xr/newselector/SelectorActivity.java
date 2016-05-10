83b3c62036d4070d4043179ec45337ee73dcc805
iamxiarui <iamxiarui@foxmail.com>
2016-05-10 19:12:19 +0800

    定义Selector的新方式

---
+++ NewSelector/src/main/java/xr/newselector/SelectorActivity.java
@@ -0,0 +1,16 @@
+package xr.newselector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 按钮选择器定义新方式
+ * */
+public class SelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selector);
+    }
+}
